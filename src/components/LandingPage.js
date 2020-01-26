8a2ddfb72bb5c7b7c945d638e102e2c15ae38e36
David Guenther <davidcarlguenther@gmail.com>
2020-01-25 18:39:32 -0600

    styled enter button

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const LandingPage = () => {
     return (
-        <div className="landing-page"></div>
+        <div className="landing-page">
+            <div className="button-container">
+                <Button className="enter-button" variant="outline-light">Enter</Button>
+            </div>
+        </div>
     )
 }
 
