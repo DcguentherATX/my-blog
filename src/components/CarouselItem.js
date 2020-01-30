30c9247db710bbc5033174ab5379ff42e0bf94ad
David Guenther <davidcarlguenther@gmail.com>
2020-01-29 20:11:17 -0600

    added styling to carousel images

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CarouselItem = (props) => {
-    console.log('props', props);
+    // console.log('props', props);
     return (
         <div className="carousel-img">
             <img className="slider-image" src={props.item.img} />
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import CarouselItem from './CarouselItem';
 
-import vespa from '../../assets/images/vespa.jpg';
-
 export default class ImageSlider extends Component {
   constructor(props) {
     super(props)
@@ -15,42 +13,15 @@ export default class ImageSlider extends Component {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         arrows: true
       };
       return (
         <div>
-          <h2> Images </h2>
+          <h2 className="slider-title">Images</h2>
           <Slider {...settings}>
             {this.props.carouselImages.map((carouselImage, index) => <CarouselItem key={index} item={carouselImage}/>)}
-            {/* <div>
-              <img className="slider-image" src={vespa} />
-            </div>
-            <div>
-              <img className="slider-image" src={vespa} />
-            </div>
-            <div>
-              <img className="slider-image" src={vespa} />
-            </div>
-            <div>
-              <img className="slider-image" src={vespa} />
-            </div>
-            <div>
-              <img className="slider-image" src={vespa} />
-            </div>
-            <div>
-              <img className="slider-image" src={vespa} />
-            </div>
-            <div>
-              <img className="slider-image" src={vespa} />
-            </div>
-            <div>
-              <img className="slider-image" src={vespa} />
-            </div>
-            <div>
-              <img className="slider-image" src={vespa} />
-            </div> */}
           </Slider>
         </div>
       );
