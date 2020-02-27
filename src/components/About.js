370dc47200708fef42342b8ce9463304961ae14c
David Guenther <davidcarlguenther@gmail.com>
2020-02-26 18:25:13 -0600

    replaced lorem ipsum with real content on About page

--- src/components/About.js
+++ src/components/About.js
@@ -25,13 +25,27 @@ const About = () => {
                 </Col>
                 <Col className="about-info" xs={12} s={12} md={12} lg={6}>
                 <p>
-                    Fugiat voluptate ullamco reprehenderit officia duis enim commodo ullamco reprehenderit voluptate fugiat mollit. Est labore aute duis incididunt occaecat. Laboris sint eu anim aliquip reprehenderit. Qui consectetur exercitation cillum occaecat minim exercitation ex exercitation consequat ad est. Sunt sint velit ex officia minim laborum sint nostrud. Officia qui cupidatat labore deserunt nulla ipsum. Aute ex consectetur quis dolor ex aliquip occaecat incididunt reprehenderit irure.
+                    My name is David Guenther and I am a recent Hack Reactor bootcamp graduate located in Austin.  I moved to 
+                    Austin from New York where I worked in event management and staffing, hospitality, and finance.  My interest
+                    in software engineering first developed in one of my previous positions where I interacted directly with a
+                    database as the main users.  Semi-annually I would collaborate with the CEO and provide ideas on how to improve
+                    the database functionality, increase efficiency, and add features.  At the time, I had no experience with software
+                    development, however, the CEO passed my suggestions on to the engineering team and they made it all happen.
+                    I was amazed that my suggestions were put into place and dramatically improved the business.
                 </p>
                 <p>
-                    Eu ipsum incididunt non nulla ad consectetur mollit proident sunt deserunt consectetur. Id aliquip voluptate cupidatat Lorem dolore mollit commodo mollit pariatur non et ad. Dolor aute laboris ex incididunt cupidatat sit. Deserunt elit tempor nisi nulla et quis cillum.
+                    Seeing my suggestions put into action is what first sparked my interest in software engineering.  I knew I had
+                    an interest in development, but I had no idea how to switch careerss from event management to engineering.  After
+                    conducting some online research, I discovered Hack Reactor, which had an extensive full stack engineering curriculum
+                    that captured my attention.
                 </p>
                 <p>
-                    Dolore ex et sit consequat id dolore laboris qui tempor duis consequat culpa. Sint eiusmod quis duis fugiat. Ullamco veniam anim nulla elit commodo. Fugiat exercitation ut excepteur consectetur commodo nostrud excepteur minim ex nostrud. Ut nisi cillum laborum irure ullamco occaecat aliqua Lorem eiusmod dolore Lorem.
+                    The Hack Reactor program was quite a challenge.  You teach yourself JavaScript before the classes begin, review the
+                    hard parts as a group, and then push forward diving into new material covering front end frameworks, servers, and databases.
+                    Upon completion of the rigorous program, I realized that I knew more about software engineering than I thought possible, but I  
+                    was also aware of the technologies, frameworks, and libraries that exist.  I created this site to practice some old skills whilst
+                    working on new ones.  Each page has a purpose, which will be briefly described at the top.  I hope you enjoy this journey as much
+                    as I have.
                 </p>
                 </Col>
             </Row>
