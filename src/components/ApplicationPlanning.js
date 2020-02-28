c714b316f8edddebd5ea423cd08dc43b6ef16fae
David Guenther <davidcarlguenther@gmail.com>
2020-02-27 18:53:10 -0600

    added lorem ipsum to first article and floating nav arrows

--- src/components/ApplicationPlanning.js
+++ src/components/ApplicationPlanning.js
@@ -1,5 +1,12 @@
 import React, { useEffect } from 'react';
 
+import welcomeSketch from '../../assets/images/sketch-welcome.jpg';
+import gallerySketch from '../../assets/images/sketch-gallery.jpg';
+import photoSketch from '../../assets/images/sketch-photo-hunt.jpg';
+import blogSketch from '../../assets/images/sketch-blog.jpg';
+import left from '../../assets/images/left-arrow.png';
+import right from '../../assets/images/right-arrow.png';
+
 const ApplicationPlanning = () => {
 
     useEffect(() => {
@@ -15,9 +22,37 @@ const ApplicationPlanning = () => {
 
     return (
         <>
-            <article className="news-article">
-                This will be an article!
-            </article>
+            <div className="article-container">
+            <div className="arrow-wrap">
+                    <img src={left} className="left" />
+                    <img src={right} className="right" />
+                </div>
+                <h1 className="article-title">Application planning, designing, and sketching</h1>
+                <article className="article-body">
+                <p>
+                    Cupidatat non culpa amet reprehenderit quis dolore minim ex irure qui ipsum ex in aliquip. Veniam velit incididunt sint tempor ipsum ipsum. Sit est proident officia cillum Lorem deserunt irure.
+                </p>
+                <p>
+                    Tempor aliqua sit eiusmod eiusmod anim proident velit incididunt dolore est. Do enim aliquip qui adipisicing qui exercitation dolor. Laborum tempor labore commodo consequat ipsum esse aliqua excepteur magna et. Culpa magna quis irure minim nulla tempor eu. Veniam quis Lorem voluptate dolore cillum dolore aliquip qui consectetur cillum.
+                </p>
+                <img className="basic-blog-img" src={welcomeSketch} alt="welcome sketch"/>
+                <p>
+                    Velit reprehenderit magna veniam tempor sit proident incididunt aliqua velit aute pariatur amet labore. Non consectetur aliquip culpa ea cupidatat fugiat cupidatat id et. Consequat ullamco fugiat elit Lorem ea proident quis fugiat exercitation nostrud. Mollit nulla in ea reprehenderit est enim.
+                </p>
+                <img className="basic-blog-img" src={gallerySketch} alt="gallery sketch"/>
+                <p>
+                    Elit laborum Lorem laboris qui eu consequat adipisicing ad consequat deserunt aute. Minim excepteur laboris ad exercitation in nulla cillum laboris. Ipsum irure labore mollit nulla dolor. Ea sit cillum fugiat cupidatat. Incididunt nulla in culpa irure laborum. Adipisicing officia deserunt magna duis. Officia consequat velit veniam ea duis.
+                </p>
+                <img className="basic-blog-img" src={photoSketch} alt="photo hunt sketch"/>
+                <p>
+                    Officia non dolor Lorem non amet sint. Adipisicing officia duis nisi cupidatat eiusmod adipisicing. Officia ad enim commodo excepteur nisi ea nisi aliquip officia aliqua. Mollit commodo cillum cillum excepteur exercitation elit reprehenderit. Amet enim pariatur magna proident excepteur esse deserunt culpa quis fugiat voluptate occaecat. Irure laborum eiusmod aliquip eu esse reprehenderit adipisicing labore nostrud nulla Lorem veniam quis aliqua.
+                </p>
+                <img className="basic-blog-img" src={blogSketch} alt="blogSketch"/>
+                <p>
+                    Ea irure proident eu veniam occaecat magna duis anim incididunt veniam. Sit ea exercitation sunt aliquip qui ut reprehenderit. Elit proident esse qui excepteur proident culpa enim laborum aliquip eu.
+                </p>
+                </article>
+            </div>
         </>
     )
 }
