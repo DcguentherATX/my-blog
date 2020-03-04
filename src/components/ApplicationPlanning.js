cf3af08da66df4e44428b99b83af521b0997da89
David Guenther <davidcarlguenther@gmail.com>
2020-03-03 20:04:09 -0600

    added content to first blog post, Application Planning

--- src/components/ApplicationPlanning.js
+++ src/components/ApplicationPlanning.js
@@ -32,30 +32,63 @@ const ApplicationPlanning = () => {
                 <h1 className="article-title">Application planning, designing, and sketching</h1>
                 <article className="article-body">
                 <p>
-                    Cupidatat non culpa amet reprehenderit quis dolore minim ex irure qui ipsum ex in aliquip. Veniam velit incididunt sint tempor ipsum ipsum. Sit est proident officia cillum Lorem deserunt irure.
+                    The purpose of this entire application was to challenge myself to learn specific technologies,
+                    styling techniques, and deployment tactics.  To begin, I needed to outline a series of pages in
+                    which I could use the techniques that I was targetting and display proper use to the viewer.
+                    Throughout this post, I have included photos of my original sketches on how I planned out design
+                    of the application.
                 </p>
                 <p>
-                    Tempor aliqua sit eiusmod eiusmod anim proident velit incididunt dolore est. Do enim aliquip qui adipisicing qui exercitation dolor. Laborum tempor labore commodo consequat ipsum esse aliqua excepteur magna et. Culpa magna quis irure minim nulla tempor eu. Veniam quis Lorem voluptate dolore cillum dolore aliquip qui consectetur cillum.
+                    I knew I wanted to create a landing page with a simple fullscreen background image that would resize
+                    responsively based on the viewer's window.  I initially wanted to use tall cacti with mountains at the
+                    page bottom.  In between, I wanted to welcome the user and have an entrance button.  My final design
+                    was pretty close to this original design.
                 </p>
                 <div className="planning-image-container">
                     <img className="basic-blog-img" src={welcomeSketch} alt="welcome sketch"/>
                     <img className="basic-blog-img" src={gallerySketch} alt="gallery sketch"/>
                 </div>
                 <p>
-                    Velit reprehenderit magna veniam tempor sit proident incididunt aliqua velit aute pariatur amet labore. Non consectetur aliquip culpa ea cupidatat fugiat cupidatat id et. Consequat ullamco fugiat elit Lorem ea proident quis fugiat exercitation nostrud. Mollit nulla in ea reprehenderit est enim.
+                    In order to practice with CSS Grid, I wanted to create a simple gallery page with visually appealing
+                    images that would scale on the size of the media screen.  In addition, I wanted to implement routing
+                    between multiple components with React Router, with the navigation and footer displayed to the screen
+                    as long as the landing page component wasn't rendered to the screen.  Designing this page was fairly
+                    quick, as I only sketched the desktop layout, deciding on the tablet and mobile layouts during
+                    implementation.
                 </p>
                 <p>
-                    Elit laborum Lorem laboris qui eu consequat adipisicing ad consequat deserunt aute. Minim excepteur laboris ad exercitation in nulla cillum laboris. Ipsum irure labore mollit nulla dolor. Ea sit cillum fugiat cupidatat. Incididunt nulla in culpa irure laborum. Adipisicing officia deserunt magna duis. Officia consequat velit veniam ea duis.
+                    Originally I had designed the Photo Hunt page as a Destinations page.  I was going to add photos to 
+                    a database, sort them by location, and display different locations in a carousel below images and content
+                    of the main destination.  After planning, I decided to perform an API request to an outside source for images
+                    and separate the database portion to a different component within the application.  The Photo Hunt page kept
+                    a similar design to the original Destinations design with a main, large image and clickable minor images
+                    located within the carousel.
+                </p>
+                <p>
+                    For the component interacting with the database, I designed an AirBnB styled dining guide for Austin.  The
+                    purpose here was to create data, store it, and filter it based on preferences.  I also thought it would be
+                    interesting to create an interactive form where users could add requests for restaurants they would like to be
+                    reviewed.  At the time I am writing this, that form has yet to be completed.  
                 </p>
                 <div className="planning-image-container">
                     <img className="basic-blog-img" src={photoSketch} alt="photo hunt sketch"/>
                     <img className="basic-blog-img" src={blogSketch} alt="blogSketch"/>
                 </div>
                 <p>
-                    Officia non dolor Lorem non amet sint. Adipisicing officia duis nisi cupidatat eiusmod adipisicing. Officia ad enim commodo excepteur nisi ea nisi aliquip officia aliqua. Mollit commodo cillum cillum excepteur exercitation elit reprehenderit. Amet enim pariatur magna proident excepteur esse deserunt culpa quis fugiat voluptate occaecat. Irure laborum eiusmod aliquip eu esse reprehenderit adipisicing labore nostrud nulla Lorem veniam quis aliqua.
+                    The final component that I wanted to work with was a blog idea, with which I could document my experience
+                    and resources throughout the process.  This would not only act as a resource for me for future development
+                    efforts, but will hopefully assist learning developers gain additional skills.  The blog was initially going
+                    to be a single page with a long list of articles, but I eventually grew to hate that idea.  I decided to create
+                    a Front Page with a newspaper feel using CSS Grid again.  Then I could link through all of the pages with
+                    React Router so each individual article could be viewed in isolation.
                 </p>
                 <p>
-                    Ea irure proident eu veniam occaecat magna duis anim incididunt veniam. Sit ea exercitation sunt aliquip qui ut reprehenderit. Elit proident esse qui excepteur proident culpa enim laborum aliquip eu.
+                    Once my planning, sketching, and designing was completed, I needed to decide on the technologies, frameworks,
+                    and libraries that I would best fit this project.  I am comfortable with React, so I wanted to bring that in and
+                    learn how to use hooks and React Router.  I wanted to bring in Bootstrap for styling assistance and overall
+                    design aesthetic.  On the backend, I wanted to use a non-relational database because I wasn't exactly sure on the
+                    structure of my data.  MongoDB would work well for that situation and I knew that I could deploy with Mongo Atlas.
+                    Once all this was organized, I moved on to creating a color scheme and implementing my design.
                 </p>
                 </article>
                 <div className="home-container">
